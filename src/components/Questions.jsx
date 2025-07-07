001a6b9f8251f1e10631f461e6a4c49f0f1349f0
Dereje Tessema <dtessema@myfuse1.education>
2025-07-07 15:51:21 -0400

    margin space added between questions

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -29,7 +29,6 @@ export default function Questions() {
         <p className="question">{he.decode(q.question)}</p>
         {options.map((option) => {
           return (
-            // <button key={option} className="choices">
             <div key={option} className="option-container">
               <input
                 id={option}
@@ -43,7 +42,6 @@ export default function Questions() {
                 {he.decode(option)}
               </label>
             </div>
-            // </button>
           );
         })}
         <hr />
